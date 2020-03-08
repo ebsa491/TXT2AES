b1568034480488849ad76dd0a415a83f4a121e0a
= <=>
2020-03-08 17:51:20 +0330

    __construct added and encryptRand and decrypt completed

--- TXT2AES.php
+++ TXT2AES.php
@@ -7,15 +7,71 @@
 
 class TXT2AES {
 
-    function encryptWithRandomKey($txt) {
+    private $encrypted = NULL; // Encrypted text
+    private $decrypted = NULL; // Decrypted and normal text
+    private $iv = NULL; // Initialization vector
+    private $key = NULL; // Encryption key
 
-    }
-
-    function encryptWithKey($txt, $key) {
+    function __construct($encrypted, $iv, $key) {
         
+        // If you want to decrypt use this when you want to create an object
+
+        $this->encrypted = $encrypted;
+        $this->iv = $iv;
+        $this->key = $key;
+
     }
 
-    function decryptWithKey($encrypted, $key) {
+    function __construct($txt) {
+
+        // If you want to encrypt use this when you want to create an object
+
+        $this->decrypted = $txt;
+    }
+
+    function encryptWithRandomKey() {
+
+        // If was success returns TRUE
+
+        if($this->decrypted != NULL) {
+
+            define('AES_256_CBC', 'aes-256-cbc');
+
+            $this->key = openssl_random_pseudo_bytes(32);
+
+            $this->iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length(AES_256_CBC));
+
+            $this->encrypted = openssl_encrypt($this->decrypted, AES_256_CBC, $key, 0, $iv);
+
+            return TRUE;
+
+        }
+
+        return FALSE;
+
+    }
+
+    function encryptWithKey($key) {
+        
+        // If was success returns TRUE
+
+    }
+
+    function decrypt() {
+
+        // If was success returns TRUE
+
+        if($this->encrypted != NULL) {
+
+            define('AES_256_CBC', 'aes-256-cbc');
+
+            $this->decrypted = openssl_decrypt($this->encrypted, AES_256_CBC, $this->key, 0, base64_decode($this->iv));
+
+            return TRUE;
+
+        }
+
+        return FALSE;
 
     }
 

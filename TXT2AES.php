404f83e242bb6910545f1d18731c96e40c326051
= <=>
2020-03-08 16:10:43 +0330

    Class created

---
+++ TXT2AES.php
@@ -0,0 +1,33 @@
+<?php
+
+// GNU Lesser General Public License (LGPL) v3.0 .
+// An open source project created by Saman Ebrahimnezhad .
+// Created at Iran (IR) .
+// PHP 7 .
+
+class TXT2AES {
+
+    private $txt;
+    private $aes;
+
+    function setTXT() {
+
+        
+
+    }
+
+    function createAndSetAES() {
+
+        
+
+    }
+
+    function getAES() {
+
+        
+
+    }
+
+}
+
+?>
